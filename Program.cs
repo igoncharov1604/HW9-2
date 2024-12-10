b06ec11af5b91b7dc25bc73e566fa603caab98ff
igoncharov1604 <i.goncharov@istu.edu.ua>
2024-12-10 18:28:19 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Лямбда-вираз у делегаті Func
+        Func<int, int, int> squareOfProduct = (a, b) => (a * b) * (a * b);
+
+        // Виклик лямбда-виразу
+        Console.Write("Введіть перше число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        // Обчислення результату
+        int result = squareOfProduct(num1, num2);
+
+        // Виведення результату
+        Console.WriteLine($"Квадрат добутку чисел {num1} і {num2} дорівнює {result}");
+    }
+}
